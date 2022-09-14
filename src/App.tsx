f7a730397f053c36f1bd376e170a30e8af4ea73c
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-13 22:42:53 -0300

    feature: fazendo um pre carregamento de um component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import {lazy, Suspense, useState} from 'react';
 
+const loadComponent = () => {
+  console.log('Fazendo um pre carregamento de um component');
+  return import('./LazyComponent');
+};
+
 const LazyLoading = lazy(() => import('./LazyComponent'));
 
 const Home = () => {
   const [show, setShow] = useState(false);
   return (
     <>
-      <button onClick={() => setShow((s) => !s)}>Aqui</button>
+      <button onClick={() => setShow((s) => !s)} onMouseOver={loadComponent}>
+        Aqui
+      </button>
       <Suspense fallback={<div>Carregando</div>}>
         {show && <LazyLoading />}
       </Suspense>
